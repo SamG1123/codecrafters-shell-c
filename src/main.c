53e800c12cdefc279087b8fbf01a65be8275f8d3
SamG1123 <157042522+SamG1123@users.noreply.github.com>
2026-02-03 15:23:12 +0530

    minor fix2

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
   char command[1024];
-
+  printf("$ ");
   fgets(command, sizeof(command), stdin);
   command[strcspn(command, "\n")] = 0;
   printf("%s: command not found\n", command);
