087c662904186e8848e17f5425a49ec7d3ee2a58
SamG1123 <157042522+SamG1123@users.noreply.github.com>
2026-02-04 13:37:59 +0530

    bug fix - 2

--- src/builtins.h
+++ src/builtins.h
@@ -6,7 +6,7 @@ void handle_echo(char **tokens, int token_count);
 void handle_type(char **tokens, int token_count, const char *path_env);
 void handle_pwd(void);
 void handle_cd(const char *path, const char *HOME);
-void handle_cat(const char *filename);
+void handle_cat(char **tokens, int token_count);
 
 extern const char *builtin_commands[];
 
--- src/main.c
+++ src/main.c
@@ -51,11 +51,10 @@ int main(int argc, char *argv[]) {
       handle_pwd();
     } else if (strcmp(tokens[0], "cd") == 0) {
       handle_cd(arg_count > 1 ? tokens[1] : "~", home_env);
+    } else if (strcmp(tokens[0], "cat") == 0) {
+      handle_cat(tokens, arg_count);
     } else if (find_file(tokens[0], path_env)) {
       execute_command(command);
-    } else if (strcmp(tokens[0], "cat") == 0) {
-      if (arg_count < 2) {
-        handle_cat(tokens[1]);
     } else {
       printf("%s: command not found\n", tokens[0]);
     }
