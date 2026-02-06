6a6ed716e7bcd7f24152a86c148abc9925cb87ac
SamG1123 <157042522+SamG1123@users.noreply.github.com>
2026-02-06 08:54:01 +0530

    codecrafters submit [skip ci]

--- src/executor.h
+++ src/executor.h
@@ -1,6 +1,8 @@
 #ifndef EXECUTOR_H
 #define EXECUTOR_H
 
+#include "shell.h"
+
 int find_file(char *command, char *path_env);
 void execute_command(char **tokens, int token_count);
 char **arg_processor(char *arg, int *argc);\
