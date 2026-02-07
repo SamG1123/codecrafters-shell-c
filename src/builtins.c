4961c0dc9875de7b8729f92c3f18e546dfb14fe8
SamG1123 <157042522+SamG1123@users.noreply.github.com>
2026-02-07 09:05:50 +0530

    codecrafters submit [skip ci]

--- src/builtins.c
+++ src/builtins.c
@@ -142,17 +142,13 @@ void handle_history(int count, char *arg, char *path_env, char *history_file) {
 
     FILE *file = fopen(history_file, "a");
     if (file != NULL) {
-      if (existing_lines == 0) {
-        fprintf(file, "history -w %s\n", history_file);
-      }
-      else {
         for (int i = 0; i < existing_lines; i++) {
           fprintf(file, "%s\n", content_buffer[i]);
         }
-        fprintf(file, "history -w %s\n", history_file);
+        //fprintf(file, "history -w %s\n", history_file);
       }
       fclose(file);
-    }
+    
 
     for (int i = 0; i < existing_lines; i++) {
       free(content_buffer[i]);
