852c593d49796b660ea5989b2e19adf6ab2468aa
SamG1123 <157042522+SamG1123@users.noreply.github.com>
2026-02-07 09:58:29 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -425,9 +425,7 @@ int main(int argc, char *argv[]) {
     }
 
     if (strcmp(tokens[0], "exit") == 0) {
-      if (using_default_history_file) {
-        remove(current_history_file);
-      }
+      
       break;
     } else if (strcmp(tokens[0], "echo") == 0) {
       handle_echo(tokens, redirect_index == -1 ? arg_count : redirect_index);
